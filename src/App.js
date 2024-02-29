7fc6402777bab142c8cff7bc2a421f635d09654a
lohit56 <lohit56@gmail.com>
2024-02-29 09:59:04 +0530

    remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Feature from "./components/Feature/Feature";
-import Footer from "./components/Footer/Footer";
-import Hero from "./components/Hero/Hero";
-import Intro from "./components/Intro/Intro";
-import Navbar from "./components/Navbar/Navbar";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Layout from "./pages/Layout";
